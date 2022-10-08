a72136bd7c8b451ac2342bc53524950e3ea2cedf
2542286041 <114057198+2542286041@users.noreply.github.com>
2022-10-08 13:54:17 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -45,7 +45,7 @@ export const config = {
     USERS: [
         {
             // 想要发送的人的名字
-            name: "自己",
+            name: "自己1",
             // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
             id: "okOuZ6JIa9lUJJWANmGng_BnO7S4",
             // 你想对他发送的模板消息的模板ID
@@ -64,7 +64,7 @@ export const config = {
         // 你可以不断按格式往下增加
         {
             // 想要发送的人的名字
-            name: "宝贝",
+            name: "自己2",
             // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
             id: "okOuZ6JIa9lUJJWANmGng_BnO7S4",
             // 你想对他发送的模板消息的模板ID
